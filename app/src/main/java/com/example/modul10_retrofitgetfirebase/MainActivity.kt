431dcd351d0abfd741562ab1a02c31e3285b5695
adns <adnisaaa@gmail.com>
2023-06-20 13:58:48 +0700

    second commit

--- app/src/main/java/com/example/modul10_retrofitgetfirebase/MainActivity.kt
+++ app/src/main/java/com/example/modul10_retrofitgetfirebase/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.modul10_retrofitgetfirebase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.modul10_retrofitgetfirebase.Fitur.Edit
+import com.example.modul10_retrofitgetfirebase.Fitur.Insert
 import com.google.firebase.database.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var userRecyclerView: RecyclerView
@@ -16,38 +22,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Inisialisasi RecyclerView
+        val insertButton: Button = findViewById(R.id.insertButton)
+        insertButton.setOnClickListener {
+            insertMahasiswa()
+        }
+
+        // Initialize RecyclerView
         userRecyclerView = findViewById(R.id.recyclerView)
         userRecyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Inisialisasi ArrayList dan Adapter
+        // Initialize ArrayList and Adapter
         userList = ArrayList()
-        adapter = MahasiswaAdapter(userList)
+        adapter = MahasiswaAdapter(userList, this, ::editMahasiswa, ::deleteMahasiswa)
         userRecyclerView.adapter = adapter
 
-        // Mendapatkan referensi database Firebase
+        // Get Firebase database reference
         database = FirebaseDatabase.getInstance().getReference("mahasiswa")
 
-        // Mendapatkan data dari Firebase
+        // Get data from Firebase
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Hapus data sebelumnya dari userList
+                // Clear previous data from userList
                 userList.clear()
 
-                // Loop melalui setiap item data dan tambahkan ke userList
+                // Loop through each data item and add it to userList
                 for (snapshot in dataSnapshot.children) {
-                    val mahasiswa =
-                        snapshot.getValue(Mahasiswa::class.java)
+                    val nim = snapshot.child("nim").getValue(String::class.java)
+                    val mahasiswa = snapshot.getValue(Mahasiswa::class.java)
                     mahasiswa?.let {
-                        userList.add(it)
+                        it.nim = nim
+                        it.nim?.let { nim -> // Check if nim is not null
+                            it.nim = nim
+                            userList.add(it)
+                        }
                     }
                 }
-                // Memperbarui RecyclerView setelah mendapatkan data baru
+
+                Log.d("DataChange", "New data received. List size: ${userList.size}")
+
+                // Update the RecyclerView after receiving new data
                 adapter.notifyDataSetChanged()
             }
+
+
             override fun onCancelled(error: DatabaseError) {
-                // Penanganan kesalahan saat mengakses Firebase
+                // Handle error when accessing Firebase
             }
         })
+    }
+
+    private fun editMahasiswa(mahasiswa: Mahasiswa) {
+        // Open the edit activity or perform the edit operation as needed
+        val intent = Intent(this, Edit::class.java)
+        intent.putExtra("nama", mahasiswa.nama)
+        intent.putExtra("nim", mahasiswa.nim)
+        intent.putExtra("telp", mahasiswa.telp)
+        startActivity(intent)
+    }
+
+    private fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        Log.d("Delete", "Delete method called for Mahasiswa: ${mahasiswa.nim}")
+
+        // Delete the mahasiswa data from Firebase
+        val userId = mahasiswa.nim
+        userId?.let {
+            database.child(it).removeValue()
+        }
+    }
+
+    private fun insertMahasiswa() {
+        val intent = Intent(this, Insert::class.java)
+        startActivity(intent)
     }
 }
